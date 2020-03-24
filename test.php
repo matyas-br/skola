b2f65bc7937ba1965cd0e3daf12ae68f691d0707
matyas-br <62599829+matyas-br@users.noreply.github.com>
2020-03-24 23:52:38 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>párek</title>
+    </head>
+    <body>
+    
+    <h2>AHOJ</h2>
+    
+        <?php
+    
+        phpinfo()
+        ?>
+ 
+    </body>
+</html>
